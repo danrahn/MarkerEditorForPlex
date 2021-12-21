621a84c047bd07f31614150a69cdd1251af7d61b
Daniel Rahn <danrahn@uw.edu>
2021-12-21 11:16:34 -0800

    Add basic edit, add, and delete functionality
    No UI entrypoints yet, but manually tested to ensure that we can
    properly add, delete, and update intro markers in the database.

--- app.js
+++ app.js
@@ -74,38 +74,163 @@ function handlePost(req, res)
     }
 
     if (req.url.startsWith('/query?')) {
-        const queryObject = url.parse(req.url,true).query;
-        let keys = queryObject.keys.split(',');
-
-        let data = {};
-        keys.forEach(key => {
-            data[key] = [];
-        });
-        let db = new sqlite3.Database(config.database);
-
-        // First, get the persistent 'intro marker' id
-        db.all("SELECT `id` FROM `tags` WHERE `tag_type`=12;", (err, rows) => {
-            const tag_id = rows[0].id;
-            let query = 'SELECT * FROM `taggings` WHERE `tag_id`=' + tag_id + ' AND (';
-            keys.forEach(key => {
-                query += '`metadata_item_id`=' + key + ' OR ';
-            });
-            query = query.substring(0, query.length - 4) + ');';
-            db.all(query, (err, rows) => {
-                rows.forEach(row => {
-                    data[row.metadata_item_id].push(row);
-                });
-
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.end(JSON.stringify(data));
-            });
-        });
-        db.close();
-        return;
+        return queryIds(req, res);
+    } else if (req.url.startsWith('/edit?')) {
+        return editMarker(req, res);
+    } else if (req.url.startsWith('/add?')) {
+        return addMarker(req, res);
+    } else if (req.url.startsWith('/delete?')) {
+        return deleteMarker(req, res);
     }
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end('{ "success" : true }');
+}
+
+function jsonError(res, code, error) {
+    res.statusCode = code;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ Error : error }));
+}
+
+function jsonSuccess(res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ success : true }));
+}
+
+function queryIds(req, res) {
+    const queryObject = url.parse(req.url,true).query;
+    let keys = queryObject.keys.split(',');
+
+    let data = {};
+    keys.forEach(key => {
+        data[key] = [];
+    });
+    let db = new sqlite3.Database(config.database);
+
+    // First, get the persistent 'intro marker' id
+    db.get("SELECT `id` FROM `tags` WHERE `tag_type`=12;", (err, row) => {
+        if (err) {
+            return jsonError(res, 400, 'Unable to find the correct tag_id');
+        }
+
+        const tag_id = row.id;
+        let query = 'SELECT * FROM `taggings` WHERE `tag_id`=' + tag_id + ' AND (';
+        keys.forEach(key => {
+            const intKey = parseInt(key);
+            if (isNaN(intKey)) {
+                return jsonError(res, 400, `Invalid key given: ${key}`);
+            }
+            query += '`metadata_item_id`=' + intKey + ' OR ';
+        });
+        query = query.substring(0, query.length - 4) + ');';
+        db.all(query, (err, rows) => {
+            if (err) {
+                return jsonError(res, 400, 'Unable to retrieve ids');
+            }
+
+            rows.forEach(row => {
+                data[row.metadata_item_id].push(row);
+            });
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(data));
+        });
+    });
+    db.close();
+}
+
+function editMarker(req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    let id = parseInt(queryObject.id);
+    let startMs = parseInt(queryObject.start);
+    let endMs = parseInt(queryObject.end);
+
+    if (isNaN(id) || isNaN(startMs) || isNaN(endMs)) {
+        return jsonError(res, 400, "invalid parameters");
+    }
+
+    let db = new sqlite3.Database(config.database);
+    db.get("SELECT * FROM `taggings` WHERE `id`=" + id + ";", (err, row) => {
+        if (err || !row || row.text != 'intro') {
+            return jsonError(res, 400, err | 'Intro marker not found');
+        }
+
+        // Use startMs.toString() to ensure we properly set '0' instead of a blank value if we're starting at the very beginning of the file
+        db.run('UPDATE `taggings` SET `time_offset`=?, `end_time_offset`=?, `thumb_url`=CURRENT_TIMESTAMP WHERE `id`=?;', [startMs.toString(), endMs, id], (err) => {
+            if (err) {
+                return jsonError(res, 400, err);
+            }
+
+            return jsonSuccess(res);
+        });
+    });
+    db.close();
+}
+
+function addMarker(req, res) {
+    // TODO: Do marker indexes have to be in order from earliest to latest? Should probably order/UPDATE regardless so it's cleaner.
+    // TODO: Client-side, detect if a new marker overlaps with another, and ask if they want to expand the existing marker instead (and check multiple overlaps)
+    const queryObject = url.parse(req.url, true).query;
+    const metadataId = parseInt(queryObject.metadataId);
+    const startMs = parseInt(queryObject.start);
+    const endMs = parseInt(queryObject.end);
+
+    let db = new sqlite3.Database(config.database);
+    db.get("SELECT `id` FROM `tags` WHERE `tag_type`=12;", (err, row) => {
+        if (err) {
+            return jsonError(res, 400, 'Unable to find the correct tag_id');
+        }
+
+        const tagId = row.id;
+        db.all("SELECT * FROM `taggings` WHERE `metadata_item_id`=? AND `tag_id`=?", [metadataId, tagId], (err, rows) => {
+            if (err) {
+                return jsonError(res, 400, err);
+            }
+
+            // There shouldn't be any gaps, but be safe and just use max + 1
+            let index = 0;
+            rows.forEach(row => {
+                console.log(row);
+                index = Math.max(index, row.index + 1);
+            });
+            
+            db.run("INSERT INTO `taggings` (`metadata_item_id`, `tag_id`, `index`, `text`, `time_offset`, `end_time_offset`, `thumb_url`, `created_at`) " +
+                        "VALUES (?, ?, ?, 'intro', ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)", [metadataId, tagId, index, startMs, endMs], (err) => {
+                if (err) {
+                    return jsonError(res, 400, err);
+                }
+
+                return jsonSuccess(res);
+            });
+        });
+    });
+    db.close();
+}
+
+function deleteMarker(req, res) {
+    // TODO: Recheck indexes on delete and adjust if necessary to avoid gaps.
+    const queryObject = url.parse(req.url, true).query;
+    const id = parseInt(queryObject.id);
+    if (isNaN(id)) {
+        return jsonError(res, 400, "Invalid marker ID");
+    }
+
+    let db = new sqlite3.Database(config.database);
+    db.get("SELECT * FROM `taggings` WHERE `id`=?", [id], (err, row) => {
+        if (err || !row || row.text != 'intro') {
+            return jsonError(res, 400, "Could not find intro marker");
+        }
+
+        db.run("DELETE FROM `taggings` WHERE `id`=?", [id], (err) => {
+            if (err) {
+                return jsonError(res, 500, 'Failed to delete intro marker');
+            }
+
+            return jsonSuccess(res);
+        });
+    });
 }
