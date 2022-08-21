f357b0076877c6934bf2bd74b55b27a5e3787430
Daniel Rahn <danrahn@uw.edu>
2022-08-21 11:58:56 -0700

    Bulk shift UI - part 3
    Add new start/end columns to bulk shift customization table, and color
    code items based on whether values are getting cut off or completely
    outside the bounds of an episode.

--- Client/Script/BulkShiftOverlay.js
+++ Client/Script/BulkShiftOverlay.js
@@ -2,7 +2,7 @@ import { Log } from "../../Shared/ConsoleLog.js";
 import { SeasonData, ShowData } from "../../Shared/PlexTypes.js";
 
 import ButtonCreator from "./ButtonCreator.js";
-import { $, $$, appendChildren, buildNode, pad0, ServerCommand, timeToMs } from "./Common.js";
+import { $, $$, appendChildren, buildNode, msToHms, pad0, ServerCommand, timeToMs } from "./Common.js";
 import Animation from "./inc/Animate.js";
 import Overlay from "./inc/Overlay.js";
 import { PlexUI, UISection } from "./PlexUI.js";
@@ -10,6 +10,7 @@ import TableElements from "./TableElements.js";
 import ThemeColors from "./ThemeColors.js";
 /** @typedef {!import('../../Shared/PlexTypes.js').ShiftResult} ShiftResult */
 /** @typedef {!import('../../Shared/PlexTypes.js').EpisodeData} EpisodeData */
+/** @typedef {!import('../../Shared/PlexTypes.js').SerializedEpisodeData} SerializedEpisodeData */
 
 /**
  * UI for bulk shifting markers for a given show/season by a set amount of time.
@@ -17,6 +18,14 @@ import ThemeColors from "./ThemeColors.js";
 class BulkShiftOverlay {
     /** @type {ShowData|SeasonData} */
     #mediaItem;
+
+    /** @type {{[episodeId: number]: SerializedEpisodeData}} */
+    #episodeData;
+
+    /**
+     * Timer id to track shift user input.
+     * @type {number} */
+    #inputTimer;
 
     /**
      * Construct a new shift overlay.
@@ -35,7 +44,13 @@ class BulkShiftOverlay {
             buildNode('hr'),
             appendChildren(buildNode('div', { id : 'shiftZone' }),
                 buildNode('label', { for : 'shiftTime' }, 'Time offset: '),
-                buildNode('input', { type : 'text', placeholder : 'ms or mm:ss[.000]', name : 'shiftTime', id : 'shiftTime' })),
+                buildNode('input', {
+                    type : 'text',
+                    placeholder : 'ms or mm:ss[.000]',
+                    name : 'shiftTime',
+                    id : 'shiftTime' },
+                    0,
+                    { keyup : this.#onTimeShiftChange.bind(this) })),
             appendChildren(buildNode('div', { id : 'bulkShiftButtons' }),
                 ButtonCreator.textButton('Apply', this.#tryApply.bind(this), { id : 'shiftApply', tooltip : 'Attempt to apply the given time shift. Brings up customization menu if any markers have multiple episodes.' }),
                 ButtonCreator.textButton('Force Apply', this.#forceApply.bind(this), { id : 'shiftForceApplyMain', class : 'shiftForceApply', tooltip : 'Force apply the given time shift to all markers, even if some episodes have multiple markers.'}),
@@ -45,6 +60,85 @@ class BulkShiftOverlay {
         );
 
         Overlay.build({ closeButton: true, forceFullscreen : true, setup : { fn : () => $('#shiftTime').focus() } }, container);
+    }
+
+    /**
+     * @param {KeyboardEvent} e */
+    #onTimeShiftChange(e) {
+        clearTimeout(this.#inputTimer);
+        const table = $('#bulkShiftCustomizeTable');
+        if (!table) {
+            return;
+        }
+
+        if (e.key == 'Enter') {
+            this.#adjustNewTimes();
+            return;
+        }
+
+        this.#inputTimer = setTimeout(this.#adjustNewTimes.bind(this), 250);
+
+    }
+
+    /**
+     * Adjust the styling of all rows in the customize table after
+     * the shift changes. */
+    #adjustNewTimes() {
+        const table = $('#bulkShiftCustomizeTable');
+        if (!table) { return; }
+
+        const shift = this.#shiftValue() || 0;
+
+        $('.bulkShiftRow', table).forEach(row => {
+            this.#styleRow(row, $$('input[type=checkbox]', row).checked, shift);
+        });
+    }
+
+    /**
+     * Adjust the styling of the new start/end values of the given row.
+     * If the start/end of the marker is getting cut off, show it in yellow
+     * If both the start/end are beyond the bounds of the episode, show both in red.
+     * If the row is unchecked, clear all styling. */
+    #styleRow(row, checked, shift=null) {
+        if (shift === null) {
+            shift = this.#shiftValue() || 0;
+        }
+
+        if (!checked) {
+            BulkShiftClasses.set(row.childNodes[4], BulkShiftClasses.Type.Reset);
+            BulkShiftClasses.set(row.childNodes[5], BulkShiftClasses.Type.Reset);
+            return;
+        }
+
+        const eid = parseInt(row.getAttribute('eid'));
+        const start = parseInt(row.getAttribute('mstart')) + shift;
+        const end = parseInt(row.getAttribute('mend')) + shift;
+        const maxDuration = this.#episodeData[eid].duration;
+        const newStart = Math.max(0, Math.min(start, maxDuration))
+        const newEnd = Math.max(0, Math.min(end, maxDuration));
+        const newStartNode = row.childNodes[4];
+        const newEndNode = row.childNodes[5];
+        newStartNode.innerText = msToHms(newStart);
+        newEndNode.innerText = msToHms(newEnd);
+        if (end < 0 || start > maxDuration) {
+            [newStartNode, newEndNode].forEach(n => {
+                BulkShiftClasses.set(n,  BulkShiftClasses.Type.Error);
+            });
+
+            return;
+        }
+
+        if (start < 0) {
+            BulkShiftClasses.set(newStartNode, BulkShiftClasses.Type.Warn);
+        } else {
+            BulkShiftClasses.set(newStartNode, BulkShiftClasses.Type.Reset);
+        }
+
+        if (end > maxDuration) {
+            BulkShiftClasses.set(newEndNode, BulkShiftClasses.Type.Warn);
+        } else {
+            BulkShiftClasses.set(newEndNode, BulkShiftClasses.Type.Reset);
+        }
     }
 
     /**
@@ -111,6 +205,7 @@ class BulkShiftOverlay {
             return;
         }
 
+        this.#episodeData = shiftResult.episodeData;
         Log.assert(shiftResult.conflict, `We should only have !applied && !conflict during check_shift, not shift. What happened?`);
         $('#bulkShiftContainer').appendChild(buildNode('h3', { id : 'resolveShiftH3' }, 'Some episodes have multiple markers, please resolve below.'));
         this.#showCustomizeTable(shiftResult);
@@ -154,7 +249,7 @@ class BulkShiftOverlay {
      * Retrieves marker information for the current metadata id and displays it in a table for the user. */
     async #check() {
         const shiftResult = await ServerCommand.checkShift(this.#mediaItem.metadataId);
-        Log.info(shiftResult, 'Got Result');
+        this.#episodeData = shiftResult.episodeData;
         this.#showCustomizeTable(shiftResult);
     }
 
@@ -226,7 +321,9 @@ class BulkShiftOverlay {
                     '',
                     'Episode',
                     TableElements.shortTimeColumn('Start Time'),
-                    TableElements.shortTimeColumn('End Time'))
+                    TableElements.shortTimeColumn('End Time'),
+                    TableElements.shortTimeColumn('New Start'),
+                    TableElements.shortTimeColumn('New End'))
             )
         )
 
@@ -242,18 +339,27 @@ class BulkShiftOverlay {
 
             const multiple = checkGroup.length > 1;
             for (const marker of checkGroup) {
+                let shift = this.#shiftValue() || 0;
                 const row = TableElements.rawTableRow(
                     getCheckbox(!multiple, marker.id, marker.episodeId),
                     `S${pad0(eInfo.seasonIndex, 2)}E${pad0(eInfo.index, 2)}`,
                     TableElements.timeData(marker.start),
-                    TableElements.timeData(marker.end)
+                    TableElements.timeData(marker.end),
+                    TableElements.timeData(marker.start),
+                    TableElements.timeData(marker.end),
                 );
+
+                row.classList.add('bulkShiftRow');
+                row.setAttribute('mstart', marker.start);
+                row.setAttribute('mend', marker.end);
+                row.setAttribute('eid', marker.episodeId);
                 if (multiple) {
                     row.classList.add('bulkShiftSemi');
                 } else {
                     row.classList.add('bulkShiftOn');
                 }
 
+                this.#styleRow(row, !multiple, shift);
                 rows.appendChild(row);
             }
         }
@@ -269,6 +375,7 @@ class BulkShiftOverlay {
         const checked = checkbox.checked;
         const linked = checkbox.getAttribute('linked') != '0';
         const row = checkbox.parentElement.parentElement.parentElement;
+        this.#styleRow(row, checked);
         if (!linked) {
             row.classList.remove(checked ? 'bulkShiftOff': 'bulkShiftOn');
             row.classList.add(checked ? 'bulkShiftOn' : 'bulkShiftOff');
@@ -304,16 +411,51 @@ class BulkShiftOverlay {
     #getIgnored() {
         const customizeTable = $('#bulkShiftCustomizeTable');
         if (!customizeTable) {
-            return { ignored : [], tableVisible : false, hasUnresolved : false };
+            return { ignored : [], tableVisible : false, hasUnresolved : false, hasCutoff : false, hasError : false };
         }
 
         const ignored = [];
-        const hasUnresolved = $('.bulkShiftSemi', customizeTable).length != 0;
+        const hasUnresolved = !!$$('tr.bulkShiftSemi', customizeTable);
 
         // Markers that are both off and 'semi' selected are ignored.
-        $('.bulkShiftOff', customizeTable).forEach(r => ignored.push(parseInt($$('input[type=checkbox]', r).getAttribute('mid'))));
-        $('.bulkShiftSemi', customizeTable).forEach(r => ignored.push(parseInt($$('input[type=checkbox]', r).getAttribute('mid'))));
-        return { ignored : ignored, tableVisible : true, hasUnresolved : hasUnresolved };
+        $('tr.bulkShiftOff', customizeTable).forEach(r => ignored.push(parseInt(r.getAttribute('mid'))));
+        $('tr.bulkShiftSemi', customizeTable).forEach(r => ignored.push(parseInt(r.getAttribute('mid'))));
+        return {
+            ignored : ignored,
+            tableVisible : true,
+            hasUnresolved : hasUnresolved,
+            hasCutoff : !!$$('td.bulkShiftSemi', customizeTable),
+            hasError  : !!$$('td.bulkShiftOff', customizeTable),
+        };
+    }
+}
+
+/**
+ * Small helper to apply styles to a table item. */
+const BulkShiftClasses = {
+    classNames : ['bulkShiftOn', 'bulkShiftOff', 'bulkShiftSemi'],
+    Type : {
+        Reset : -1,
+        Error :  1,
+        Warn  :  2,
+    },
+    /**
+     * Set the class of the given node.
+     * @param {HTMLTableCellElement} node
+     * @param {number} idx BulkShiftClasses.Type value */
+    set : (node, idx) => {
+        const names = BulkShiftClasses.classNames;
+        if (idx == -1) {
+            node.classList.remove(names[1]);
+            node.classList.remove(names[2]);
+            return;
+        }
+
+        if (!node.classList.contains(names[idx])) {
+            for (let i = 0; i < names.length; ++i) {
+                i == idx ? node.classList.add(names[i]) : node.classList.remove(names[i]);
+            }
+        }
     }
 }
 
